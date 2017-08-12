4a71e7548718a6011eeb7574e3e42cf2dd153693
Shelly Tang <shellytang@gmail.com>
2017-08-11 22:57:56 -0700

    created book detail container

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducers-books';
+import ActiveBook from './reducers-active-book';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
