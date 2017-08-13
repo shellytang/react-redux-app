832c66d10c7b7e4a8a309a1c8753518b249a1163
Shelly Tang <shellytang@gmail.com>
2017-08-12 22:37:22 -0700

    ajax request for weather api working

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {term: ''};
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+
   }
 
   onInputChange(event) {
@@ -14,7 +19,8 @@ export default class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
-    // we need to go and fetch weather data
+    this.props.fetchWeather(this.state.term); // this calls our action creator
+    this.setState({term: ''}); // resets back to empty string
   }
   render() {
     return (
@@ -31,3 +37,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
