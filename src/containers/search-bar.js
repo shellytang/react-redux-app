11d96d1d9e8fca4dce02218bf209325ce6fd57e8
Shelly Tang <shellytang@gmail.com>
2017-08-12 21:34:38 -0700

    created form elements

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value })
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    // we need to go and fetch weather data
+  }
   render() {
     return (
-      <form className='input-group'>
-        <input />
+      <form className='input-group' onSubmit={this.onFormSubmit}>
+        <input
+          placeholder='Get a five-day forecast in your favorite cities'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange}/>
         <span className='input-group-btn'>
           <button type='submit' className='btn bt-secondary'>Submit</button>
         </span>
